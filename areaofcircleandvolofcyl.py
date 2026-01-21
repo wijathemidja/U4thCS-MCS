a969eae16d80e1fb1586d4bfa572d504330fb65a
Rahul Wijesurendra <18rwijesurendra@localhost>
2026-01-21 12:52:43 +0000

    fixed it

--- areaofcircleandvolofcyl.py
+++ areaofcircleandvolofcyl.py
@@ -2,6 +2,6 @@ def areacircle(r):
     a = ((r**2) * 3.14159265358979323)
     return (a)
     
-def volumecylinder(r,h)
+def volumecylinder(r,h):
     v=((r**2)*3.14159265358979323*h)
     return (v)
